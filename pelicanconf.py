02ebdfbd6184539c74c9378f888ef652460d572f
Bob Belderbos <bobbelderbos@gmail.com>
2021-04-16 10:36:32 +0200

    forgot slug

--- pelicanconf.py
+++ pelicanconf.py
@@ -94,7 +94,7 @@ NEST_ARCHIVES_HEADER_TITLE = 'Archive'
 
 default_pages = ['index', 'authors', 'categories', 'tags', 'archives']
 pybites_pages = ("apply talk build community confidence courses devtools friends "
-                 "hacktoberfest marketing pareto pbcall resources sscall "
+                 "hacktoberfest marketing pareto thanks pbcall resources sscall "
                  "techblog workshop coaching productivity entrepreneur "
                  "tips 100days").split()
 DIRECT_TEMPLATES = default_pages + pybites_pages
